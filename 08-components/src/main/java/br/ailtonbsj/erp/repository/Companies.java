4e267524e3e131b247e6b898ef51ef2fdfcce448
ailtonbsj <ailton.ifce@gmail.com>
2023-05-31 11:42:03 -0300

    Using JSF components

---
+++ 08-components/src/main/java/br/ailtonbsj/erp/repository/Companies.java
@@ -0,0 +1,47 @@
+package br.ailtonbsj.erp.repository;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
+import br.ailtonbsj.erp.model.Company;
+
+public class Companies implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Inject
+	private EntityManager manager;
+	
+	public Companies() {
+	}
+	
+	public Companies(EntityManager manager) {
+		this.manager = manager;
+	}
+
+	public Company byId(Long id) {
+		return manager.find(Company.class, id);
+	}
+	
+	public List<Company> fetch(String name) {
+		// Using JPQL
+		TypedQuery<Company> q = manager
+				.createQuery("from Company where fantasyName like :fantasyName", Company.class);
+		q.setParameter("fantasyName", name + "%");
+		return q.getResultList();
+	}
+	
+	public Company save(Company company) {
+		return manager.merge(company);
+	}
+	
+	public void remove(Company company) {
+		company = byId(company.getId());
+		manager.remove(company);
+	}
+
+}
