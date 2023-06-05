bde5b655117d125ae82819ae26f7369dc757dc13
ailtonbsj <ailton.ifce@gmail.com>
2023-06-05 19:56:21 -0300

    using datatble

---
+++ 13-primefaces-datatable/src/main/java/br/ailtonbsj/erp/model/ActivityBranch.java
@@ -0,0 +1,59 @@
+package br.ailtonbsj.erp.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "activity_branch")
+public class ActivityBranch implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false, length = 80)
+	private String description;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ActivityBranch other = (ActivityBranch) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+}
