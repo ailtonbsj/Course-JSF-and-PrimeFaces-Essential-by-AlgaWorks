f6ced128274ee8b3ac401ebd9967945fc31f7e4a
ailtonbsj <ailton.ifce@gmail.com>
2023-06-13 00:49:10 -0300

    using validation

---
+++ 20-validation/src/main/java/br/ailtonbsj/erp/repository/ActivitiesBraches.java
@@ -0,0 +1,38 @@
+package br.ailtonbsj.erp.repository;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+import br.ailtonbsj.erp.model.ActivityBranch;
+
+public class ActivitiesBraches implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Inject
+	private EntityManager manager;
+	
+	public ActivitiesBraches() {
+	}
+
+	public ActivitiesBraches(EntityManager manager) {
+		this.manager = manager;
+	}
+
+	public List<ActivityBranch> fetch(String description) {
+		// Using Criteria
+		CriteriaBuilder cb = manager.getCriteriaBuilder();
+		CriteriaQuery<ActivityBranch> cq = cb.createQuery(ActivityBranch.class);
+		Root<ActivityBranch> root = cq.from(ActivityBranch.class);
+		cq.select(root);
+		cq.where(cb.like(root.get("description"), description + "%"));
+		TypedQuery<ActivityBranch> q = manager.createQuery(cq);
+		return q.getResultList();
+	}
+}
