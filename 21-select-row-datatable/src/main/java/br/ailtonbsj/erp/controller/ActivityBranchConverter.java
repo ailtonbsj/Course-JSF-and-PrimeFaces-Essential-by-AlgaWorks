443095c03ee1659e1085b5542d976875285ea610
ailtonbsj <ailton.ifce@gmail.com>
2023-06-14 10:14:29 -0300

    selecing row on datatable

---
+++ 21-select-row-datatable/src/main/java/br/ailtonbsj/erp/controller/ActivityBranchConverter.java
@@ -0,0 +1,36 @@
+package br.ailtonbsj.erp.controller;
+
+import java.util.List;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+
+import br.ailtonbsj.erp.model.ActivityBranch;
+
+public class ActivityBranchConverter implements Converter {
+
+	private List<ActivityBranch> activityBranchList;
+	
+	public ActivityBranchConverter(List<ActivityBranch> activityBranchList) {
+		this.activityBranchList = activityBranchList;
+	}
+
+	@Override
+	public Object getAsObject(FacesContext context, UIComponent component, String value) {
+		ActivityBranch obj = null;
+		for (ActivityBranch activityBranch : activityBranchList) {
+			if(activityBranch.getId().toString().equals(value))
+				obj = activityBranch;
+		}
+		return obj;
+	}
+
+	@Override
+	public String getAsString(FacesContext context, UIComponent component, Object value) {
+		if(value == null) return null;
+		ActivityBranch activityBranch = (ActivityBranch) value;
+		return activityBranch.getId().toString();
+	}
+
+}
---
+++ 21-select-row-datatable/src/main/java/br/ailtonbsj/erp/controller/BusinessManagementBean.java
@@ -0,0 +1,126 @@
+package br.ailtonbsj.erp.controller;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.faces.convert.Converter;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.primefaces.context.RequestContext;
+
+import br.ailtonbsj.erp.model.ActivityBranch;
+import br.ailtonbsj.erp.model.Company;
+import br.ailtonbsj.erp.model.CompanyType;
+import br.ailtonbsj.erp.repository.ActivitiesBraches;
+import br.ailtonbsj.erp.repository.Companies;
+import br.ailtonbsj.erp.service.RegisterCompanyService;
+import br.ailtonbsj.erp.util.FacesMessages;
+
+/*
+ * TYPES OF SCOPED IN JSF
+ * 
+ * @RequestScoped from javax.enterprise.context.RequestScoped
+ * @ViewScoped from javax.faces.view.ViewScoped
+ * @SessionScoped from javax.enterprise.context.SessionScoped
+ * @ApplicationScoped from javax.enterprise.context.ApplicationScoped
+ * */
+
+@Named
+@ViewScoped
+public class BusinessManagementBean implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Inject
+	private Companies companies;
+	
+	@Inject
+	private FacesMessages messages;
+	
+	@Inject
+	private ActivitiesBraches activitiesBraches;
+	
+	@Inject
+	private RegisterCompanyService registerCompanyService;
+	
+	private Converter activityBranchConverter;
+	
+	private List<Company> listOfCompanies;
+	
+	private String searchTerm;
+	
+	private Company company;
+	
+	private boolean hadSearch() {
+		return searchTerm != null && !searchTerm.equals("");
+	}
+	
+	public void prepareNewCompany() {
+		company = new Company();
+	}
+	
+	public void save() {
+		registerCompanyService.save(company);
+		if(hadSearch()) fetch();
+		else listAllCompanies();
+		messages.info("Empresa cadastrada com sucesso!");
+		RequestContext.getCurrentInstance().update(
+				Arrays.asList("frm:companiesDataTable", "frm:messages"));
+	}
+		
+	public void fetch() {
+		listOfCompanies = companies.fetch(searchTerm);
+		if(listOfCompanies.isEmpty()) {
+		}
+		messages.info("Consulta não retornou registros.");
+	}
+	
+	public void listAllCompanies() {
+		listOfCompanies = companies.listAll();
+	}
+
+	public List<Company> getListOfCompanies() {
+		return listOfCompanies;
+	}
+
+	public void setListOfCompanies(List<Company> listOfCompanies) {
+		this.listOfCompanies = listOfCompanies;
+	}
+	
+	public String getSearchTerm() {
+		return searchTerm;
+	}
+	
+	public void setSearchTerm(String searchTerm) {
+		this.searchTerm = searchTerm;
+	}
+	
+	public CompanyType[] getCompanyTypes() {
+		return CompanyType.values();
+	}
+	
+	public List<ActivityBranch> completeActivityBranch(String term) {
+		List<ActivityBranch> list = activitiesBraches.fetch(term);
+		activityBranchConverter = new ActivityBranchConverter(list);
+		return list;
+	}
+
+	public Converter getActivityBranchConverter() {
+		return activityBranchConverter;
+	}
+	
+	public Company getCompany() {
+		return company;
+	}
+	
+	public void setCompany(Company company) {
+		this.company = company;
+	}
+	
+	public boolean isCompanySelected() {
+		return company != null && company.getId() != null;
+	}
+}
